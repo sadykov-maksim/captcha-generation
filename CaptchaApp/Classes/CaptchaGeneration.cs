273e65c6748e331205d7b07036a460aaf4c727fc
Avocado <sadykov-maksim-2003@yandex.ru>
2023-05-07 19:16:19 +0500

    Complication of captcha

--- CaptchaApp/Classes/CaptchaGeneration.cs
+++ CaptchaApp/Classes/CaptchaGeneration.cs
@@ -44,12 +44,19 @@ namespace CaptchaApp.Classes
         /// <param name="textBlock">Объект текстового блока</param>
         /// <param name="algorithm">Алгоритм</param>
         /// <returns>Текстовый блок с уникальными свойствами</returns>
-        public TextBlock TBlock(TextBlock textBlock, char algorithm, Random random)
+        public TextBlock TBlock(TextBlock textBlock, char algorithm, Random random, Brush brush)
         {
             textBlock.Text += algorithm;
             textBlock.FontSize = random.Next(9, 18);
             textBlock.Margin = new Thickness(4, 4, 4, 4);
-            textBlock.Foreground = Brushes.White;
+            // Поворот капчи
+            textBlock.RenderTransformOrigin = new Point(0.5, 0.5); // Центральная точка поворота
+            SkewTransform skewTransform = new SkewTransform(random.Next(0, 35), 0);
+            textBlock.RenderTransform = skewTransform;
+            RotateTransform rotateTransform = new RotateTransform(random.Next(-35, 35));
+            textBlock.RenderTransform = rotateTransform;
+            //
+            textBlock.Foreground = brush;
             int weights = random.Next(1, 7);
             switch (weights)
             {
@@ -92,6 +99,7 @@ namespace CaptchaApp.Classes
             Random rand = new Random();
             Color randomColor = Color.FromRgb((byte)rand.Next(256), (byte)rand.Next(256), (byte)rand.Next(256));
             panel.Background = new SolidColorBrush(randomColor);
+
             string capitalAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
             string normalAlphabet = "abcdefghijklmnopqrstuvwxyz";
             string numbers = "1234567890";
@@ -99,6 +107,8 @@ namespace CaptchaApp.Classes
             int length = rand.Next(8, 16);
             for (int i = 0; i < length; i++)
             {
+                Color randomColor2 = Color.FromRgb((byte)rand.Next(256), (byte)rand.Next(256), (byte)rand.Next(256));
+                Brush fillingText = new SolidColorBrush(randomColor2);
                 TextBlock textBlock = new TextBlock();
                 int index = rand.Next(capitalAlphabet.Length);
                 int index2 = rand.Next(normalAlphabet.Length);
@@ -108,24 +118,24 @@ namespace CaptchaApp.Classes
                 switch (state)
                 {
                     case 0:
-                        TBlock(textBlock, capitalAlphabet[index], rand);
+                        TBlock(textBlock, capitalAlphabet[index], rand, fillingText);
                         panel.Children.Add(textBlock);
                         captcha += capitalAlphabet[index];
                         break;
                     case 1:
-                        TBlock(textBlock, normalAlphabet[index2], rand);
+                        TBlock(textBlock, normalAlphabet[index2], rand, fillingText);
                         panel.Children.Add(textBlock);
                         captcha += normalAlphabet[index2];
                         break;
                     case 2:
                       
-                        TBlock(textBlock, numbers[index3], rand);
+                        TBlock(textBlock, numbers[index3], rand, fillingText);
                         panel.Children.Add(textBlock);
                         captcha += numbers[index3];
                         break;
                     case 3:
                        
-                        TBlock(textBlock, symbols[index4], rand);
+                        TBlock(textBlock, symbols[index4], rand, fillingText);
                         panel.Children.Add(textBlock);
                         captcha += symbols[index4];
                         break;
